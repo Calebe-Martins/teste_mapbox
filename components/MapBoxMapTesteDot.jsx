a4944746c2cde2cefae32bdd7da1cbe82c69ae00
Calebe Martins <calebe.graciano@gmail.com>
2023-04-08 18:43:59 -0300

    Testado no cell

--- components/MapBoxMapTesteDot.jsx
+++ components/MapBoxMapTesteDot.jsx
@@ -24,7 +24,7 @@ const MapboxMap = () => {
       zoom: 14,
     });
 
-    // add click listener to map TESTAR ISSO NO CELULAR
+    // add click listener to map
     map.current.on("click", (event) => {      
       if(map.current.getLayer("circle")) {
         map.current.removeLayer("circle")
