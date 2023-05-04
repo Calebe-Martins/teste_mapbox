2b72f4d75f679190d6c23e1b91b52cc81451a7b4
Martins <calebe@aglet.com.br>
2023-05-04 19:48:14 -0300

    vercel n deixava dar build sem a key da api

--- components/functions/supabase.jsx
+++ components/functions/supabase.jsx
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://zlnlpintvtfvjrxczyls.supabase.co'
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpsbmxwaW50dnRmdmpyeGN6eWxzIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODMxMzE2NDMsImV4cCI6MTk5ODcwNzY0M30.KxuIzsjWyRozW5C_xYo5zZxvA9fV1wo-GV8GwXi7QfA'
 
 const supabase = createClient(supabaseUrl, supabaseKey)
 
