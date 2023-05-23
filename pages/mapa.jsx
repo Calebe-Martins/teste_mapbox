54a37ecc47a1921742c95c7cdad889b38067fbb1
Calebe Martins <calebe.graciano@gmail.com>
2023-05-23 11:00:03 -0300

    Mapa comentado

--- pages/mapa.jsx
+++ pages/mapa.jsx
@@ -1,5 +1,173 @@
-import supabase from "../components/functions/supabase"
-import MapBoxMap from "../components/MapBoxMap"
+// import supabase from "../components/functions/supabase"
+// import MapBoxMap from "../components/MapBoxMap"
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import { useRouter } from "next/router";
+// import { useState, useEffect } from 'react';
+
+// const Mapa = () => {
+//   const router = useRouter();
+//   const { uid } = router.query;
+
+//   const [show, setShow] = useState(false);
+//   const [items, setItems] = useState([]);
+
+//   const handleClose = () => setShow(false);
+//   const handleShow = () => setShow(true);
+
+//   async function getPromo() {
+
+
+//     const { data, error } = await supabase
+//     .from('promocao')
+//     .select('*')
+//     .eq('uid', uid)
+//     .eq('ativo', true)
+
+//     if (error) {
+//       console.error(error);
+//       return;
+//     }
+
+//     if (data && data.length > 0) {
+//       console.table(data)
+//       setItems(data)
+//     }
+//   }
+
+//   async function abreModalPegaPromo() {
+//     getPromo()
+//     handleShow()
+//   }
+  
+//   async function handleCadastro() {
+//     const titulo = document.getElementById("descCurta").value;
+//     const desc = document.getElementById("descLonga").value;
+
+//     // Verifica se o campo "titulo" ou "descCurta" estão vazios
+//     if (!titulo || !desc) {
+//       console.error("Preencha todos os campos!");
+//       return;
+//     }
+
+//     await supabase
+//     .from('promocao')
+//     .insert([
+//       { uid: uid, descCurta: titulo, descLonga: desc },
+//     ])
+//     .then(() => {
+//       console.log("Promoção cadastrada com sucesso!");
+//       getPromo()
+
+//       document.getElementById("descCurta").value = ""
+//       document.getElementById("descLonga").value = ""
+//     })
+//     .catch((error) => {
+//       console.log(error)
+//     });
+//   }
+
+//   const handleDeleteItem = async (index) => {
+//     const itemToDelete = items[index];
+
+//     // Atualiza a coluna "ativo" para false
+//     await supabase
+//       .from('promocao')
+//       .update({ ativo: false })
+//       .eq('id', itemToDelete.id);
+
+//     // Remove o item do array local
+//     const updatedItems = [...items];
+//     updatedItems.splice(index, 1);
+//     setItems(updatedItems);
+//   };
+
+  
+//   return (
+//     <>
+//       <div className="position-relative">
+//           <MapBoxMap uid={uid} />
+//       </div>
+//       <div className="position-absolute top-0 start-0 p-3">
+//           <button className="btn btn-primary" onClick={abreModalPegaPromo}>
+//               Promoçoes
+//           </button>
+//       </div>
+
+//       {show && (
+//       <div className="modal" tabIndex={-1} role="dialog" style={{ display: 'block' }}>
+//         <div className="modal-dialog" role="document">
+//           <div className="modal-content">
+//             <div className="modal-header">
+//               <h5 className="modal-title">Cadastro de promoção</h5>
+//               <button type="button" className="close" onClick={handleClose}>
+//                 <span aria-hidden="true">&times;</span>
+//               </button>
+//             </div>
+//             <div className="modal-body">
+//               <div className="row">
+//                 <div className="col-lg-12 form-group">
+//                   <label>Titulo</label>
+//                   <input className="form-control" type="text" id="descCurta" />
+//                 </div>
+//                 <div className="col-lg-12 form-group mt-3">
+//                   <label>Descrição</label>
+//                   <textarea className="form-control" type="text" id="descLonga" />
+//                 </div>
+//               </div>
+//               <div className="mt-4" style={{ height: '200px', overflow: 'auto' }}>
+//                 <ul className="list-group">
+//                   {/* Usando a função map para renderizar cada item em um loop */}
+//                   {items.map((item, index) => (
+//                     <li className="list-group-item  d-flex justify-content-between align-items-center" key={index}>
+//                       <span>{item.descCurta}</span>
+//                       <button
+//                         onClick={() => handleDeleteItem(index)} // Substitua handleDeleteItem pela função que você deseja chamar ao clicar no botão
+//                         className="delete-button close"
+//                       >
+//                         X
+//                       </button>
+//                     </li>
+//                   ))}
+//                 </ul>
+//               </div>
+//             </div>
+//             <div className="modal-footer">
+//               <button type="button" className="btn btn-secondary" onClick={handleClose}>
+//                 Fechar
+//               </button>
+//               <button type="button" className="btn btn-primary" onClick={handleCadastro}>
+//                 Salvar
+//               </button>
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//       )}
+//       {show && <div className="modal-backdrop fade show"></div>}
+
+//       <style jsx>{`
+//         .close {
+//           font-size: 1.5rem;
+//           font-weight: bold;
+//           color: red;
+//           padding: 0;
+//           background: none;
+//           border: none;
+//           outline: none;
+//           /* Adicione outros estilos personalizados aqui */
+//         }
+        
+//       `}</style>
+
+//     </>
+    
+//   );
+// };
+
+// export default Mapa;
+
+import supabase from "../components/functions/supabase";
+import MapBoxMap from "../components/MapBoxMap";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { useRouter } from "next/router";
 import { useState, useEffect } from 'react';
@@ -15,13 +183,11 @@ const Mapa = () => {
   const handleShow = () => setShow(true);
 
   async function getPromo() {
-
-
     const { data, error } = await supabase
-    .from('promocao')
-    .select('*')
-    .eq('uid', uid)
-    .eq('ativo', true)
+      .from('promocao')
+      .select('*')
+      .eq('uid', uid)
+      .eq('ativo', true);
 
     if (error) {
       console.error(error);
@@ -29,16 +195,20 @@ const Mapa = () => {
     }
 
     if (data && data.length > 0) {
-      console.table(data)
-      setItems(data)
+      console.table(data);
+      setItems(data);
     }
   }
 
+  useEffect(() => {
+    getPromo();
+  }, []);
+
   async function abreModalPegaPromo() {
-    getPromo()
-    handleShow()
+    getPromo();
+    handleShow();
   }
-  
+
   async function handleCadastro() {
     const titulo = document.getElementById("descCurta").value;
     const desc = document.getElementById("descLonga").value;
@@ -49,99 +219,100 @@ const Mapa = () => {
       return;
     }
 
-    await supabase
-    .from('promocao')
-    .insert([
-      { uid: uid, descCurta: titulo, descLonga: desc },
-    ])
-    .then(() => {
-      console.log("Promoção cadastrada com sucesso!");
-      getPromo()
+    try {
+      await supabase
+        .from('promocao')
+        .insert([{ uid: uid, descCurta: titulo, descLonga: desc }]);
 
-      document.getElementById("descCurta").value = ""
-      document.getElementById("descLonga").value = ""
-    })
-    .catch((error) => {
-      console.log(error)
-    });
+      console.log("Promoção cadastrada com sucesso!");
+      getPromo();
+
+      document.getElementById("descCurta").value = "";
+      document.getElementById("descLonga").value = "";
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   const handleDeleteItem = async (index) => {
     const itemToDelete = items[index];
 
     // Atualiza a coluna "ativo" para false
-    await supabase
-      .from('promocao')
-      .update({ ativo: false })
-      .eq('id', itemToDelete.id);
+    try {
+      await supabase
+        .from('promocao')
+        .update({ ativo: false })
+        .eq('id', itemToDelete.id);
 
-    // Remove o item do array local
-    const updatedItems = [...items];
-    updatedItems.splice(index, 1);
-    setItems(updatedItems);
+      // Remove o item do array local
+      const updatedItems = [...items];
+      updatedItems.splice(index, 1);
+      setItems(updatedItems);
+    } catch (error) {
+      console.log(error);
+    }
   };
 
-  
   return (
     <>
       <div className="position-relative">
-          <MapBoxMap uid={uid} />
+        <MapBoxMap uid={uid} />
       </div>
       <div className="position-absolute top-0 start-0 p-3">
-          <button className="btn btn-primary" onClick={abreModalPegaPromo}>
-              Promoçoes
-          </button>
+        <button className="btn btn-primary" onClick={abreModalPegaPromo}>
+          Promoções
+        </button>
       </div>
 
       {show && (
-      <div className="modal" tabIndex={-1} role="dialog" style={{ display: 'block' }}>
-        <div className="modal-dialog" role="document">
-          <div className="modal-content">
-            <div className="modal-header">
-              <h5 className="modal-title">Cadastro de promoção</h5>
-              <button type="button" className="close" onClick={handleClose}>
-                <span aria-hidden="true">&times;</span>
-              </button>
-            </div>
-            <div className="modal-body">
-              <div className="row">
-                <div className="col-lg-12 form-group">
-                  <label>Titulo</label>
-                  <input className="form-control" type="text" id="descCurta" />
+        <div className="modal" tabIndex={-1} role="dialog" style={{ display: 'block' }}>
+          <div className="modal-dialog" role="document">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h5 className="modal-title">Cadastro de promoção</h5>
+                <button type="button" className="close" onClick={handleClose}>
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <div className="modal-body">
+                <div className="row">
+                  <div className="col-lg-12 form-group">
+                    <label>Título</label>
+                    <input className="form-control" type="text" id="descCurta" />
+                  </div>
+                  <div className="col-lg-12 form-group mt-3">
+                    <label>Descrição</label>
+                    <textarea className="form-control" type="text" id="descLonga" />
+                  </div>
                 </div>
-                <div className="col-lg-12 form-group mt-3">
-                  <label>Descrição</label>
-                  <textarea className="form-control" type="text" id="descLonga" />
+                <div className="mt-4" style={{ height: '200px', overflow: 'auto' }}>
+                  <ul className="list-group">
+                    {/* Usando a função map para renderizar cada item em um loop */}
+                    {items.map((item, index) => (
+                      <li className="list-group-item  d-flex justify-content-between align-items-center" key={index}>
+                        <span>{item.descCurta}</span>
+                        <button
+                          onClick={() => handleDeleteItem(index)} // Substitua handleDeleteItem pela função que você deseja chamar ao clicar no botão
+                          className="delete-button close"
+                        >
+                          X
+                        </button>
+                      </li>
+                    ))}
+                  </ul>
                 </div>
               </div>
-              <div className="mt-4" style={{ height: '200px', overflow: 'auto' }}>
-                <ul className="list-group">
-                  {/* Usando a função map para renderizar cada item em um loop */}
-                  {items.map((item, index) => (
-                    <li className="list-group-item  d-flex justify-content-between align-items-center" key={index}>
-                      <span>{item.descCurta}</span>
-                      <button
-                        onClick={() => handleDeleteItem(index)} // Substitua handleDeleteItem pela função que você deseja chamar ao clicar no botão
-                        className="delete-button close"
-                      >
-                        X
-                      </button>
-                    </li>
-                  ))}
-                </ul>
+              <div className="modal-footer">
+                <button type="button" className="btn btn-secondary" onClick={handleClose}>
+                  Fechar
+                </button>
+                <button type="button" className="btn btn-primary" onClick={handleCadastro}>
+                  Salvar
+                </button>
               </div>
-            </div>
-            <div className="modal-footer">
-              <button type="button" className="btn btn-secondary" onClick={handleClose}>
-                Fechar
-              </button>
-              <button type="button" className="btn btn-primary" onClick={handleCadastro}>
-                Salvar
-              </button>
             </div>
           </div>
         </div>
-      </div>
       )}
       {show && <div className="modal-backdrop fade show"></div>}
 
@@ -156,11 +327,9 @@ const Mapa = () => {
           outline: none;
           /* Adicione outros estilos personalizados aqui */
         }
-        
       `}</style>
 
     </>
-    
   );
 };
 
